021e343914293e25bf60fdad9e0a486d4190a388
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-14 07:59:07 +0700

    Update authors.js

--- authors.js
+++ authors.js
@@ -1,14 +1,8 @@
 module.exports = [
   {
-    id: 'mazipan',
-    name: 'Irfan Maulana',
-    link: 'https://mazipan.space/',
+    id: 'iamvue',
+    name: 'iamVue',
+    link: 'https://iamvue.com',
     avatar: 'https://avatars2.githubusercontent.com/u/7221389?s=100&v=4'
-  },
-  {
-    id: 'mandaputtra',
-    name: 'Manda Putra',
-    link: 'https://mandaputtra.github.io',
-    avatar: 'https://avatars2.githubusercontent.com/u/23342943?s=460&v=4'
   }
 ]
