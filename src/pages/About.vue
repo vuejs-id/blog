1e4c2a0f898f334d89ef5c84ff760bbce23edefc
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-14 16:21:57 +0700

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,60 +1,118 @@
 <template>
-  <Layout :show-logo="false">
-    <!-- Author intro -->
-    <Author :show-title="true" />
-
-    <!-- List posts -->
-    <div class="posts">
-     
-     <p>
-        iamVue adalah blog yang membahas seputar pemrograman web. Kami menyediakan ebrbagai tips dan tutorial yang mungkin bisa kalian jadikan referensi.
-     </p>
-     
+  <Layout>
+    <div class="post-title">
+      <h1 class="post-title__text">
+        About Us
+      </h1>
+      <PostMeta :post="$page.post" />
     </div>
+    <div class="post content-box">
+
+      <div class="post__content"/>
+
+     iamVue adalah blog yang membahas seputar pemrograman web. Kami menyediakan ebrbagai tips dan tutorial yang mungkin bisa kalian jadikan referensi.
+        
+      </div>
+    </div>
+
+    <div class="post-comments">
+      <!-- Add comment widgets here -->       
+    </div>
+
+    <Author class="post-author" />
   </Layout>
 </template>
 
-<page-query>
-query {
-  posts: allPost(filter: { published: { eq: true }}) {
-    edges {
-      node {
-        id
-        title
-        date (format: "D MMM YYYY")
-        timeToRead
-        description
-        cover_image (width: 770, height: 380, blur: 10)
-        path
-        author {
-          id
-          name
-          link
-          title
-          avatar
-          path
+<script>
+import PostMeta from '~/components/PostMeta'
+import PostTags from '~/components/PostTags'
+import Author from '~/components/Author.vue'
+export default {
+  components: {
+    Author,
+    PostMeta,
+    PostTags
+  },
+  metaInfo() {
+    return {
+      title: this.$page.post.title,
+      meta: [
+        {
+          name: 'description',
+          content: this.$page.post.description
         }
-        tags {
-          id
-          title
-          path
-        }
-      }
+      ]
     }
+  }
+}
+</script>
+
+<page-query>
+query Post ($id: ID!) {
+  post: post (id: $id) {
+    title
+    path
+    date (format: "D MMM YYYY")
+    timeToRead
+    author {
+      id
+      name
+      link
+      title
+      avatar
+      path
+    }
+    tags {
+      id
+      title
+      path
+    }
+    description
+    content
+    cover_image (width: 860, blur: 10)
   }
 }
 </page-query>
 
-<script>
-import Author from '~/components/Author.vue'
-import PostCard from '~/components/PostCard.vue'
-export default {
-  components: {
-    Author,
-    PostCard
-  },
-  metaInfo: {
-    title: 'Belajar Pemrograman Web'
+<style lang="scss">
+.post-title {
+  padding: calc(var(--space) / 2) 0 calc(var(--space) / 2);
+  text-align: center;
+}
+.post {
+  &__header {
+    width: calc(100% + var(--space) * 2);
+    margin-left: calc(var(--space) * -1);
+    margin-top: calc(var(--space) * -1);
+    margin-bottom: calc(var(--space) / 2);
+    overflow: hidden;
+    border-radius: var(--radius) var(--radius) 0 0;
+    img {
+      width: 100%;
+    }
+    &:empty {
+      display: none;
+    }
+  }
+  &__content {
+    h2:first-child {
+      margin-top: 0;
+    }
+    img {
+      width: calc(100% + var(--space) * 2);
+      margin-left: calc(var(--space) * -1);
+      display: block;
+      max-width: none;
+    }
   }
 }
-</script>
+.post-comments {
+  padding: calc(var(--space) / 2);
+  &:empty {
+    display: none;
+  }
+}
+.post-author {
+  margin-top: calc(var(--space) / 2);
+}
+</style>
