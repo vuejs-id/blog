e6eaad08c928a2d708345dda48ddc2031bbd1739
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-14 18:04:14 +0700

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="social__links">
+  <a href="https://www.jobstreet.co.id/id/job-search/job-vacancy.php?ojs=10&key=vue.js" target="_blank">Job</a>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+<style lang="scss">
+.footer__vue {
+  margin-bottom: 1em;
+  a {
+    margin: 0 0.5em;
+  }
+}
+</style>
