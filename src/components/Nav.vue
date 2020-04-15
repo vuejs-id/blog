a08e36b1b39942e1f0fbfbbb4d2e4ce207efbc68
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-15 09:37:40 +0700

    Create Nav.vue

---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav__vue">
+  <a href="https://www.iamvue.com/tag/vuejs/"><i class="fab fa-vuejs"></i> Vue.js</a> <a href="https://www.iamvue.com/tag/php/"><i class="fab fa-php"></i> PHP</a> <a href="https://www.iamvue.com/tag/git/"><i class="fab fa-git-alt"></i> Git</a>
+  <a href="https://www.iamvue.com/tag/html/"><i class="fab fa-html5"></i> Html</a> <a href="https://www.iamvue.com/tag/laravel/"><i class="fab fa-laravel"></i> Laravel</a> <a href="https://www.iamvue.com/tag/nodejs/"><i class="fab fa-node-js"></i> Node.js</a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+<style lang="scss">
+.nav__vue {
+  margin-bottom: 1em;
+  a {
+    margin: 0 0.5em;
+  }
+}
+</style>
