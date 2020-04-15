205b7b02c33c79aa195de79b5ffb40cb6b9e0c24
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-15 08:43:28 +0700

    Update SocialLinks.vue

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -41,7 +41,7 @@
         ></path>
       </svg>
     </a>
-    <a href="https://iamvue/" title="Website">
+    <a href="https://iamvue.com/" title="Website">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
