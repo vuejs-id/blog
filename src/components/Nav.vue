efb7954d0226c8acfaf2b22571e1988c062e9b95
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-15 09:48:28 +0700

    Update Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav__vue">
-  <a href="https://www.iamvue.com/tag/vuejs/"><i class="fab fa-vuejs"></i> Vue.js</a> <a href="https://www.iamvue.com/tag/php/"><i class="fab fa-php"></i> PHP</a> <a href="https://www.iamvue.com/tag/git/"><i class="fab fa-git-alt"></i> Git</a>
-  <a href="https://www.iamvue.com/tag/html/"><i class="fab fa-html5"></i> Html</a> <a href="https://www.iamvue.com/tag/laravel/"><i class="fab fa-laravel"></i> Laravel</a> <a href="https://www.iamvue.com/tag/nodejs/"><i class="fab fa-node-js"></i> Node.js</a>
+  <a href="https://www.iamvue.com/tag/vuejs/">🔥 Vue.js</a> <a href="https://www.iamvue.com/tag/php/">🔥 PHP</a> <a href="https://www.iamvue.com/tag/git/">🔥 Git</a>
+  <a href="https://www.iamvue.com/tag/html/">🔥 Html</a> <a href="https://www.iamvue.com/tag/laravel/">🔥 Laravel</a> <a href="https://www.iamvue.com/tag/nodejs/">🔥 Node.js</a>
   </div>
 </template>
 <script>
