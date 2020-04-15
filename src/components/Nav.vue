df8a4a9219d3fd902c6c7c1d26e1bd83ff727119
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-15 11:04:30 +0700

    Update Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="nav__vue">
-  <a href="https://www.iamvue.com/tag/vuejs/" class="post-tags__nav"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg> Vue.js</a> 
-  <a href="https://www.iamvue.com/tag/php/" class="post-tags__nav">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg> PHP</a> 
-  <a href="https://www.iamvue.com/tag/git/"class="post-tags__nav">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg> Git</a>
-  <a href="https://www.iamvue.com/tag/html/"class="post-tags__nav">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg> Html</a> 
-  <a href="https://www.iamvue.com/tag/laravel/" class="post-tags__nav">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg> Laravel</a> 
-  <a href="https://www.iamvue.com/tag/nodejs/"class="post-tags__nav">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg> Node.js</a>
-  <a href="https://www.iamvue.com/tag/wordpress/"class="post-tags__nav">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg> Wordpress</a>
+  <a href="https://www.iamvue.com/tag/vuejs/" class="post-tags__nav"> Vue.js</a> 
+  <a href="https://www.iamvue.com/tag/php/" class="post-tags__nav"> PHP</a> 
+  <a href="https://www.iamvue.com/tag/git/"class="post-tags__nav"> Git</a>
+  <a href="https://www.iamvue.com/tag/html/"class="post-tags__nav"> Html</a> 
+  <a href="https://www.iamvue.com/tag/laravel/" class="post-tags__nav"> Laravel</a> 
+  <a href="https://www.iamvue.com/tag/nodejs/"class="post-tags__nav"> Node.js</a>
+  <a href="https://www.iamvue.com/tag/wordpress/"class="post-tags__nav"> Wordpress</a>
   </div>
 </template>
-<script src="https://kit.fontawesome.com/715a166ac7.js" crossorigin="anonymous"></script>
 <script>
 export default {
   name: 'Nav'
@@ -36,10 +29,5 @@ text-align: center;
     text-decoration: none;
   }
   
-}
-.post-tags__nav {
-  background: #fff;
-  padding: 5px;
-  border-radius: 5px;
 }
 </style>
