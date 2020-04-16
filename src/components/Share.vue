022b1b085906ca104ae360f88bade379a65bee2f
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-16 09:00:06 +0700

    Create Share.vue

---
+++ src/components/Share.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="share__vue">
+  <a href="https://id.jooble.org/lowongan-kerja-programmer" target="_blank">📋 Job</a> <a href="https://projects.co.id/public/browse_projects/listing?a=ae4311" target="_blank">💻 Freelance Job</a>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'Share'
+}
+</script>
+<style lang="scss">
+.share__vue {
+  margin-bottom: 1em;
+  a {
+    margin: 0 0.5em;
+    text-decoration: none;
+  }
+}
+</style>
