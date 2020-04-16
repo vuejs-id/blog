38283dda4a3d52766c31be8df58516f089567540
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-16 09:02:32 +0700

    Update Share.vue

--- src/components/Share.vue
+++ src/components/Share.vue
@@ -1,7 +1,60 @@
 <template>
   <div class="share__vue">
-  <a href="https://id.jooble.org/lowongan-kerja-programmer" target="_blank">📋 Job</a> <a href="https://projects.co.id/public/browse_projects/listing?a=ae4311" target="_blank">💻 Freelance Job</a>
-    
+  <social-sharing url="https://iamvue.com/"
+                      title="Tutorial Vue.js"
+                      description="Belajar Vue.js dan belajar pemrograman web"
+                      quote="Ayo belajar Vue.js."
+                      hashtags="vuejs, javascript, framework, php"
+                      twitter-user="iamvue"
+                      inline-template>
+  <div>
+      <network network="email">
+          <i class="fa fa-envelope"></i> Email
+      </network>
+      <network network="facebook">
+        <i class="fa fa-facebook"></i> Facebook
+      </network>
+      <network network="googleplus">
+        <i class="fa fa-google-plus"></i> Google +
+      </network>
+      <network network="line">
+        <i class="fa fa-line"></i> Line
+      </network>
+      <network network="linkedin">
+        <i class="fa fa-linkedin"></i> LinkedIn
+      </network>
+      <network network="odnoklassniki">
+        <i class="fa fa-odnoklassniki"></i> Odnoklassniki
+      </network>
+      <network network="pinterest">
+        <i class="fa fa-pinterest"></i> Pinterest
+      </network>
+      <network network="reddit">
+        <i class="fa fa-reddit"></i> Reddit
+      </network>
+      <network network="skype">
+        <i class="fa fa-skype"></i> Skype
+      </network>
+      <network network="sms">
+        <i class="fa fa-commenting-o"></i> SMS
+      </network>
+      <network network="telegram">
+        <i class="fa fa-telegram"></i> Telegram
+      </network>
+      <network network="twitter">
+        <i class="fa fa-twitter"></i> Twitter
+      </network>
+      <network network="vk">
+        <i class="fa fa-vk"></i> VKontakte
+      </network>
+      <network network="weibo">
+        <i class="fa fa-weibo"></i> Weibo
+      </network> 
+      <network network="whatsapp">
+        <i class="fa fa-whatsapp"></i> Whatsapp
+      </network>
+  </div>
+</social-sharing> 
   </div>
 </template>
 <script>
