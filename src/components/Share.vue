2064fc8e9f021278fe378687eb7c2295bbed9b52
DevSourceID <35456489+devsourceid@users.noreply.github.com>
2020-04-16 08:50:35 +0700

    Create Share.vue

---
+++ src/components/Share.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="share__vue">
+  <div class="sharethis-inline-share-buttons"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Share'
+}
+</script>
+<style lang="scss">
+.share__vue {
+  margin-bottom: 1em;
+  a {
+    margin: 0 0.5em;
+  }
+}
+</style>
