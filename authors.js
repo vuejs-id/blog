79582365cda058966fe9613cd45dd1b8bb9ae3f3
Jovi <redzjovi@gmail.com>
2020-10-17 21:50:34 +0700

    Update author.js.

--- authors.js
+++ authors.js
@@ -3,19 +3,24 @@ module.exports = [
     id: 'mazipan',
     name: 'Irfan Maulana',
     link: 'https://mazipan.space/',
-    avatar: 'https://avatars2.githubusercontent.com/u/7221389?s=100&v=4'
+    avatar: 'https://avatars2.githubusercontent.com/u/7221389?s=100&v=4',
   },
   {
     id: 'mandaputtra',
     name: 'Manda Putra',
     link: 'https://mandaputtra.github.io',
-    avatar: 'https://avatars2.githubusercontent.com/u/23342943?s=460&v=4'
+    avatar: 'https://avatars2.githubusercontent.com/u/23342943?s=460&v=4',
   },
   {
     id: 'ridomulanda',
     name: 'Rido Mulanda',
     link: 'https://penerbangkode.github.io',
-    avatar:
-      'https://avatars2.githubusercontent.com/u/56765525?s=88&u=7f58fd6fbe506b7bd24d667fcb4226d4c0f14f03&v=4'
-  }
+    avatar: 'https://avatars2.githubusercontent.com/u/56765525?s=88&u=7f58fd6fbe506b7bd24d667fcb4226d4c0f14f03&v=4',
+  },
+  {
+    id: 'redzjovi',
+    name: 'Jovi',
+    link: 'https://github.com/redzjovi',
+    avatar: 'https://avatars0.githubusercontent.com/u/18608616?s=300&u=425bed8e2597d0e7ff566b9dfb85e79b57582ee9&v=4',
+  },
 ]
